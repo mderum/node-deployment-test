30ba6b89a095c4ad40da8e4ac65a23b5d772abcb
Md Erum <mderum3@gmail.com>
2022-12-17 22:55:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const log =console.log
 const app= express()
 const hbs = require('hbs')
 
-const port= process.env.port || 3000
+const port= process.env.PORT || 3000
 
 //static dir path
 const staticPath = path.join(__dirname,"../public")
